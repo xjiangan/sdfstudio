50bc5406b7568a459777501b4daa0ba18f27c618
CHI Ziheng <chiziheng15@gmail.com>
2023-04-16 17:45:37 +0200

    modify point_projectors

--- nerfstudio/model_components/point_projectors.py
+++ nerfstudio/model_components/point_projectors.py
@@ -15,13 +15,13 @@ class PointProjectors(nn.Module):
                 depth: TensorType[..., 1],
                 additional_inputs: Dict[str, TensorType]) -> Tuple[TensorType[..., 2], TensorType[..., 3]]:
         world_positions = ray_bundle.origins + ray_bundle.directions * depth
-        ref_cameras = additional_inputs["ref_cameras"]
+        ref_cameras = additional_inputs["ref_cameras"].to("cuda:0")
         ref_cameras_c2w = ref_cameras.camera_to_worlds
         fx, fy = ref_cameras.fx, ref_cameras.fy
         cx, cy = ref_cameras.cx, ref_cameras.cy
-        R, t = ref_cameras_c2w[:, :3], ref_cameras_c2w[:, 3]
-        ref_positions = (R.T @ world_positions.T - R.T @ t).T
-        x_coords = fx * ref_positions[:, 0] / (-ref_positions[:, 2]) + cx
-        y_coords = fy * ref_positions[:, 1] / (-ref_positions[:, 2]) + cy
+        R, t = ref_cameras_c2w[..., :3], ref_cameras_c2w[..., 3]
+        ref_positions = (R.permute(0,2,1) @ world_positions.unsqueeze(2) - R.permute(0,2,1) @ t.unsqueeze(2)).squeeze()
+        x_coords = fx * ref_positions[:, [0]] / (-ref_positions[:, [2]]) + cx
+        y_coords = fy * ref_positions[:, [1]] / (-ref_positions[:, [2]]) + cy
         ref_pixels = torch.concatenate((x_coords, y_coords), dim=1)
         return ref_pixels, world_positions
