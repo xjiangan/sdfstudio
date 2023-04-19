fdf64e621d6ff8dd9e61222e133698c8875647ba
CHI Ziheng <chiziheng15@gmail.com>
2023-04-20 00:21:25 +0200

    reprojection and disparity ongoing

--- nerfstudio/model_components/point_projectors.py
+++ nerfstudio/model_components/point_projectors.py
@@ -11,23 +11,20 @@ class PointProjectors(nn.Module):
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, ray_bundle: RayBundle,
-                depth: TensorType[..., 1],
-                additional_inputs: Dict[str, TensorType]) -> Tuple[TensorType[..., 2], TensorType[..., 3]]:
-
-        # Source pixels -> World space
-        world_positions = ray_bundle.origins + ray_bundle.directions * depth
+    def forward(self,
+                world_positions: TensorType[..., 3],
+                cameras: Cameras) -> Tuple[TensorType[..., 2], TensorType[..., 3]]:
 
         # World space -> Reference cameras space
-        ref_cameras = additional_inputs["ref_cameras"].to("cuda:0")
-        ref_cameras_c2w = ref_cameras.camera_to_worlds # (num_rays, 3, 4)
-        fx, fy = ref_cameras.fx, ref_cameras.fy
-        cx, cy = ref_cameras.cx, ref_cameras.cy
-        R, t = ref_cameras_c2w[..., :3], ref_cameras_c2w[..., 3]
-        ref_positions = (R.permute(0,2,1) @ world_positions.unsqueeze(2) - R.permute(0,2,1) @ t.unsqueeze(2)).squeeze()
+        cameras = cameras.to("cuda:0")
+        cameras_c2w = cameras.camera_to_worlds # (num_rays, 3, 4)
+        fx, fy = cameras.fx, cameras.fy
+        cx, cy = cameras.cx, cameras.cy
+        R, t = cameras_c2w[..., :3], cameras_c2w[..., 3]
+        positions = (R.permute(0,2,1) @ world_positions.unsqueeze(2) - R.permute(0,2,1) @ t.unsqueeze(2)).squeeze()
 
         # Reference cameras space -> Reference pixels
-        x_coords = fx * ref_positions[:, [0]] / (-ref_positions[:, [2]]) + cx
-        y_coords = fy * ref_positions[:, [1]] / (-ref_positions[:, [2]]) + cy
+        x_coords = fx * positions[:, [0]] / (-positions[:, [2]]) + cx
+        y_coords = fy * positions[:, [1]] / (-positions[:, [2]]) + cy
         ref_pixels = torch.concatenate((x_coords, y_coords), dim=1)
-        return ref_pixels, world_positions
+        return ref_pixels, -positions[:, [2]]
